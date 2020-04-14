5540fd56072f1c01489103ba9bca3e7f3670c528
DezignNuts <63385599+DezignNuts@users.noreply.github.com>
2020-04-14 18:55:11 -0500

    Update numbers.cpp comments

--- individual/numbers.cpp
+++ individual/numbers.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int myNum; //initial myNum value
 int myAge; //declare constant myAge
-int a, b, x, y;
-string myUser;
+int a, b, x, y; //declare operator ints
+string myUser; //declare user name
 
 int math1(int a, int b) { //This function does addition
 	int ad;
@@ -31,17 +31,16 @@ int main() {
 	cout << "What is your favorite number?\n";
 	cin >> myNum; //gets favorite number
 	cout << "How old are you?\n";
-	cin >> myAge;
+	cin >> myAge; //gets user age
 	cout << "```````````````````````````````````````````````````````````````````````````````\n";
 
-	
-
-	cout << "Hello " << myUser <<"!\n"; //First cout line
+	cout << "Hello " << myUser <<"!\n"; //Greeting
 	cout << "This is the information you gave me:\n\n";
 	cout << "Your favorite number is: " << myNum << "\n"; //Tells the user myNum is favorite number
 	cout << "You are currently " << myAge << " years old!\n\n"; //Tells the user myAge
 	cout << "````````````````````````````````````````````````````````````````````````````````\n";
-	cout << "Now it's time for Random Facts!\n\n";
+	
+	cout << "Now it's time for Random Facts!\n\n"; //Gathers 2 numbers for the math functions
 	cout << "Give me a number:\n";
 	int numberOne;
 	cin >> numberOne;
@@ -49,14 +48,15 @@ int main() {
 	int numberTwo;
 	cin >> numberTwo;
 
-	int answerOne;
+	int answerOne;         //Sends the numbers to the math functions
 	answerOne = math1(numberOne, numberTwo);
 	int answerTwo;
 	answerTwo = math2(numberOne, numberTwo);
 
+	//Gives outputs
 	cout << "Did you know that "<< numberOne <<" plus "<< numberTwo <<" equals " << answerOne << "?\n";
 	cout << "Did you also know that " << numberOne << " times " << numberTwo << " equals " << answerTwo << "?\n";
 	
-	system("pause");
-	return (0);
+	system("pause"); //press any key to continue...
+	return (0); //end main loop
 }
